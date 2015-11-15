5a9e5e2b3f707fca364bbc56316697386185f5fb
JonatanOrue <jonatanorue@gmail.com>
2015-11-15 20:09:54 -0200

    Funcionario classes.

--- src/br/ufms/bean/Funcionario.java
+++ src/br/ufms/bean/Funcionario.java
@@ -1,48 +1,88 @@
 package br.ufms.bean;
 
-public abstract class Funcionario{
-	
-	private int codigo;
+import br.ufms.dao.daoCliente;
+import br.ufms.dao.daoFuncionario;
+
+public class Funcionario{
+
 	private String cpf;
 	private String login;
 	private String senha;
 	private String nome;
+	private daoFuncionario df;
 	
 	public Funcionario(){
-		this.codigo = 0;
 		this.cpf = "";
 		this.login = "";
 		this.senha = "";
 		this.nome = "";
+		
+		this.df = new daoFuncionario(this);
 	}
 	
-	public Funcionario(String nome, int codigo, String cpf, String login, String senha){
-		this.codigo = codigo;
+	public Funcionario(String nome, String cpf, String login, String senha){
 		this.cpf = cpf;
 		this.login = login;
 		this.senha = senha;
 		this.nome = nome;
+		
+		this.df = new daoFuncionario(this);
 	}
 	
 	public void cadastrarFuncionario(){
+		
+		if(df.salvar()){
+			System.out.println("Funcionario inserido com sucesso!");
+		}else{
+			System.out.println("Funcionario já cadastrado!");
+		}
 		
 	}
 	
 	public void buscarFuncionario(){
 		
+		if(df.buscar()){
+			System.out.println("Funcionario existente!");
+		}else{
+			System.out.println("Funcionario não encontrado!");
+		}
+		
 	}
 	
-	public abstract void excluirFuncionario();
-	
-	public abstract void editarFuncionario();
-	
-	
-	public int getCodigo() {
-		return codigo;
+	public void excluirFuncionario(){
+		
+		if(df.excluir()){
+			System.out.println("Funcionario excluído com sucesso!");
+		}else{
+			System.out.println("Funcionario não existente!");
+		}
+		
 	}
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
+	
+	public void editarFuncionario(){
+		
 	}
+	
+	public int getCodigoFuncionario(){
+		return df.getId();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public daoFuncionario getDf() {
+		return df;
+	}
+
+	public void setDf(daoFuncionario df) {
+		this.df = df;
+	}
+
 	public String getCpf() {
 		return cpf;
 	}
--- src/br/ufms/dao/daoFuncionario.java
+++ src/br/ufms/dao/daoFuncionario.java
@@ -1,10 +1,91 @@
 package br.ufms.dao;
 
-import br.ufms.bean.Funcionario;;
-public class daoFuncionario {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-	 public void save (Funcionario func);    
-	  public void delete (Funcionario func);    
-	  public List list ();    
-	  public User find (String nome);
+import br.ufms.bean.Funcionario;
+
+public class daoFuncionario {
+	private final Connection connection;
+	private Funcionario f;
+
+	 public daoFuncionario(Funcionario f){
+		 connection = new ConnectionFactory().getConnection();
+		 this.f = f;	 
+	 }
+	 
+	 public boolean salvar (){
+			try {
+				Statement stmt = connection.createStatement();
+				String sqlConsulta = "SELECT * FROM Funcionario where cpf = '" + f.getCpf() + "';";
+				ResultSet rs = stmt.executeQuery(sqlConsulta);
+				if (rs.next()) {
+					return false;
+				}
+				
+				String sqlInsert = "INSERT INTO Funcionario (cpf, login, senha, nome) VALUES(?, ?, ?, ?);";
+				PreparedStatement ps = connection.prepareStatement(sqlInsert);
+				ps.setString(1, f.getCpf());
+				ps.setString(2, f.getLogin());
+				ps.setString(3, f.getSenha());
+				ps.setString(4, f.getNome());
+				
+				ps.execute();
+				
+				return true;
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		public boolean buscar(){
+			try {
+				Statement stmt = connection.createStatement();
+				String sqlConsulta = "SELECT * FROM Funcionario where cpf = '" + f.getCpf() + "';";
+				ResultSet rs = stmt.executeQuery(sqlConsulta);
+				if (rs.next()) {
+					return true;
+				}else {
+					return false;
+				}	
+			}catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		public boolean excluir(){
+			try {
+					Statement stmt = connection.createStatement();
+					String sqlConsulta = "SELECT * FROM Funcionario where cpf = '" + f.getCpf() + "';";
+					ResultSet rs = stmt.executeQuery(sqlConsulta);
+					if (!rs.next()) {
+						return false;
+					}else{
+					
+						String sqlDelete = "DELETE FROM Funcionario WHERE cpf = ?;";
+						PreparedStatement ps = connection.prepareStatement(sqlDelete);
+						ps.setString(1, f.getCpf());
+						
+						ps.execute();
+						return true;
+					}
+			}catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		public int getId(){
+			try {
+				Statement stmt = connection.createStatement();
+				String sqlConsulta = "SELECT (idFuncionario) FROM Funcionario where cpf = '" + f.getCpf() + "';";
+				ResultSet rs = stmt.executeQuery(sqlConsulta);
+				return rs.getInt("idCFuncionario");
+					
+			}catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			
+		}
 }
